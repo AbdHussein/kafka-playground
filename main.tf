8d6681a52199f4d088807ecddd52f2cb2ac08c45
AbdHussein <abdulrahmaanhussein97@gmail.com>
2024-03-09 15:30:05 +0200

    fix(deployment): remove the remote configuration for terraform

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# The configuration for the `remote` backend.
-terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "example-org-fe328c"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "getting-started"
-    }
-  }
-}
-
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
