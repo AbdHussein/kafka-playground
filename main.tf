3c8147b037a0ecf9504c0c8b5866e6e007c66ec5
AbdHussein <abdulrahmaanhussein97@gmail.com>
2024-03-14 00:03:06 +0200

    feat(deployemnt): adding new VPC using terraform

--- main.tf
+++ main.tf
@@ -11,9 +11,21 @@ provider "google" {
   region  = var.region
 }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
+data "google_compute_zones" "this" {
+  region  = var.region
+  project = var.project
+}
+
+locals {
+  type  = ["public", "private"]
+  zones = data.google_compute_zones.this.names
+}
+
+# VPC
+resource "google_compute_network" "this" {
+  name                            = "${var.project}-vpc"
+  delete_default_routes_on_create = false
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+
 }
