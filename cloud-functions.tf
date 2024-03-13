34a49428de35c1d7756e0470b611434009fc7ffa
AbdHussein <abdulrahmaanhussein97@gmail.com>
2024-03-13 20:25:20 +0200

    feat(deployment): add new terraform cloud-function module

---
+++ cloud-functions.tf
@@ -0,0 +1,21 @@
+resource "google_cloudfunctions_function" "function" {
+  name        = "${var.data-project}-function"
+  description = "a sample cloud founction created by terraform"
+  runtime     = "nodejs16"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
